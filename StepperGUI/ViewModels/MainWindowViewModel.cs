24942be5486ce0a0798cd90d20f42117d706cbe7
alanbonoft <alan.bonoft@gmail.com>
2026-04-13 17:02:38 +0100

    Initial commit

---
+++ StepperGUI/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace StepperGUI.ViewModels
+{
+    public partial class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting { get; } = "Welcome to Avalonia!";
+    }
+}
---
+++ StepperGUI/ViewModels/ViewModelBase.cs
@@ -0,0 +1,8 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace StepperGUI.ViewModels
+{
+    public abstract class ViewModelBase : ObservableObject
+    {
+    }
+}
